4402aa807d9a46e28b7f601ac4eb04909b32c0ff
unknown <ajharalishaikh@gmail.com>
2022-11-30 16:02:36 +0100

    installable file snowflake color

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='snowflake',
+    packages=['snowflake'],
+    description='snowflake with different color',
+    version='0.1',
+    url='https://github.com/Ajharali-Shaikh15039/SnowFlakes',
+    author='Ajharali Shaikh',
+    author_email='ajharalishaikh@gmail.com',
+    keywords=['pip','snowflake']
+    )
